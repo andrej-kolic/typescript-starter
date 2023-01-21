7f14a8be3ac5d815a608b6b233455f7b6f6cdf9e
Rey <andrej.kolic@gmail.com>
2023-01-21 18:56:32 +0100

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,11 @@
-// module.exports = {
-//   transform: {
-//     '^.+\\.ts?$': 'ts-jest',
-//   },
-//   testEnvironment: 'node',
-//   testRegex: './src/.*\\.(test|spec)?\\.(ts|ts)$',
-//   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-//   roots: ['<rootDir>/src'],
-// };
-
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
   testEnvironment: 'node',
+  testRegex: './src/.*\\.(test|spec)?\\.(ts|ts)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  roots: ['<rootDir>/src'],
 };
